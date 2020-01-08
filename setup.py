730b4b377f99ea79b59845e426f417e91af6828b
Sjoerd Kerkstra <sjoerd.kerkstra@radboudumc.nl>
2020-01-08 11:16:15 +0100

    Resources are now correctly included in install

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     keywords='dicomgenerator',
     name='dicomgenerator',
-    packages=find_packages(include=['dicomgenerator', 'dicomgenerator.*']),
+    packages=find_packages(include=['dicomgenerator']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
