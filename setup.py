9e61572b2820de96710d244cadbb6e88e43f5e75
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-04-06 17:26:08 +0200

    Fixes security problem

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Pillow==6.2.1', 'numpy==1.18.0', 'pydicom==1.4.0.dev0',
+requirements = ['Pillow==6.2.2', 'numpy==1.18.0', 'pydicom==1.4.0.dev0',
                 'matplotlib==3.1.2', 'factory-boy==2.12.0']
 
 setup_requirements = ['pytest-runner', ]
