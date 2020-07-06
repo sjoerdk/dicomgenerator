5ed753b76f05ef97f50103b08d09fe0077141719
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2020-07-06 10:28:29 +0200

    Bump version: 0.3.6 → 0.3.7

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/sjoerdk/dicomgenerator',
-    version='0.3.6',
+    version='0.3.7',
     zip_safe=False,
 )
