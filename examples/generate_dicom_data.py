76617baf0af73a347484225dabdda532dbb0f26e
sjoerdk <sjoerd.kerkstra@radboudumc.nl>
2023-11-20 16:41:57 +0100

    updates pre-commit flake-8

--- examples/generate_dicom_data.py
+++ examples/generate_dicom_data.py
@@ -1,3 +1,4 @@
+import uuid
 from pathlib import Path
 
 from dicomgenerator.export import export
@@ -10,9 +11,11 @@ output_dir = Path("/tmp/dummy_dicom")
 
 def generate_some_dicom_files(output_dir):
     output_dir.mkdir(parents=True, exist_ok=True)
-    filenames = [f"dcmfile{x}" for x in range(5)]
+    filenames = [f"dcmfile{x}" for x in range(5, 10)]
     for filename in filenames:
-        export(dataset=CTDatasetFactory(), path=output_dir / filename)
+        export(
+            dataset=CTDatasetFactory(PatientID="patient2"), path=output_dir / filename
+        )
 
     print(f"Wrote {len(filenames)} files to {output_dir}")
 
@@ -31,5 +34,28 @@ def generate_file_with_specifics(output_path):
     print(f"Wrote file to {output_path}")
 
 
-# generate_file_with_specifics(output_path=output_dir / 'dicomfile_m')
-generate_some_dicom_files(output_dir)
+def generate_dicom_structure(structure, output_dir):
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    count = 0
+    for patient, studies in structure.items():
+        for study, seriess in studies.items():
+            for series in seriess:
+                for _ in range(2):
+                    count = count + 1
+                    export(
+                        dataset=CTDatasetFactory(
+                            PatientID=patient,
+                            StudyInstanceUID=study,
+                            SeriesInstanceUID=series,
+                        ),
+                        path=output_dir / str(uuid.uuid4()),
+                    )
+
+    print(f"Wrote {count} files to {output_dir}")
+
+
+structure = {
+    "patient1": {"11111.1": ["2222.1", "2222.2"], "11111.2": ["2222.1"]},
+    "patient2": {"11111.1": ["2222.1"], "11111.2": ["2222.1", "2222.2", "2222.3"]},
+}
